34569737e5940e4135625d48f96552e71c567098
sunilregmi233 <sunilregmi223@gmail.com>
2025-08-10 12:26:31 -0700

    initial commit

--- measure/admin.py
+++ measure/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import SeedImage, SeedMeasurement
 # Register your models here.
+admin.site.register(SeedImage)
+admin.site.register(SeedMeasurement)
