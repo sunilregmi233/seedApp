612efe13e38a94fe1716b7530992fe0bf41026c6
sunilregmi233 <sunilregmi223@gmail.com>
2025-08-10 12:15:31 -0700

    initial commit

--- seedmeasure/settings.py
+++ seedmeasure/settings.py
@@ -134,6 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For collectstatic
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Your local static folder (optional)
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
